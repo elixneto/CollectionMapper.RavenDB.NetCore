44c55db22ec49dee3b08f6465a7604597e11ba50
Élix Neto <elixneto@gmail.com>
2026-05-12 12:12:22 -0400

    consoleapp removed

--- src/ConsoleApp/Entities/Unmapped.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConsoleApp.Entities;
-
-public class Unmapped
-{
-    public string Id { get; set; }
-    public string Name { get; set; } = "Unmapped info";
-}
--- src/ConsoleApp/Entities/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConsoleApp.Entities;
-
-internal class User
-{
-    public string Id { get; set; }
-    private string OtherStringId { get; set; } = Guid.NewGuid().ToString();
-}
